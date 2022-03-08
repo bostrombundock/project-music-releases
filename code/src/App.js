e39e64ee4d96ecf9c356033adb22a0cb523b2d58
frida axelsson <axelsson.frida@gmail.com>
2022-03-08 15:43:04 +0100

    added album name

--- code/src/App.js
+++ code/src/App.js
@@ -2,15 +2,27 @@ import React from 'react'
 import data from './data.json'
 
 // import {Album} from './components/Album'
+// import {Artist} from './compontents/Artist'
 
 import Album from './components/Album'
-
 console.log(data)
 
 export const App = () => {
   return (
     <section>
-      <Album/>
+      <Album name = {data.albums.items[0].name}
+      description = "This is a test"
+      artist = {data.albums.items[0].artists[0].name}
+      // img = src{=data.albums.items[0].images[0].url}
+      />
+      {/* <Artist/> */}
+      {/* <Album title="Testy Mc Testyface igen !"
+      description = "massor med viktig text"
+      name = {data.albums.items[1].name}/>/> */}
+      
     </section>
+    
+   
   )
 }
+
--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,30 +1,32 @@
 import React from "react";
-
-// export const Album = () => {
-// return (
-//     <section className='Container'>
-//     <div>
-//       <h1>Testy Mc Testyface!</h1>
-//       <p>This is a test</p>
-//       <p>Testy testy test</p>
-//       <img src="https://www.placecage.com/200/300" alt="Nicolas Cage"/>
-//     </div>
+//  export const Album = () => {
+  //  return (
+    //      <section className='Container'>
+    //      <div>
+//        <h1>Testy Mc Testyface!</h1>
+//        <p>This is a test</p>
+//        <p>Testy testy test</p>
+//        <img src="https://www.placecage.com/200/300" alt="Nicolas Cage"/>
+//      </div>
 //   </section>
-// )
-// };
+//  )
+//  };
 
 
-const Album = () => {
-    return (
-        <section className='Container'>
-        <div>
-          <h1>Testy Mc Testyface!</h1>
-          <p>This is a test</p>
-          <p>Testy testy test</p>
+const Album = (props) => {
+  console.log(props)
+  return (
+         <section className='Container'>
+         <div>
+           <h1>{props.name}</h1>
+           <p>{props.description}</p>
+           <p>{props.artist}</p>
           <img src="https://www.placecage.com/200/300" alt="Nicolas Cage"/>
-        </div>
-      </section>
-    )
-    };
+          <img src="{props.img}" alt="Nicolas Cage"/>
+         </div>
+       </section>
+     )
+     };
     
-    export default Album;
+     export default Album;
+    
