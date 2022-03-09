875a68434d913f23513cd9cd0d31878b7fefbcf3
frida axelsson <axelsson.frida@gmail.com>
2022-03-09 16:19:13 +0100

    display img,artist and album name

--- code/src/App.js
+++ code/src/App.js
@@ -5,20 +5,19 @@ import data from './data.json'
 // import {Artist} from './compontents/Artist'
 
 import Album from './components/Album'
-console.log(data)
+
+console.log('data',data)
+
 
 export const App = () => {
   return (
     <section>
-      <Album name = {data.albums.items[0].name}
-      description = "This is a test"
-      artist = {data.albums.items[0].artists[0].name}
-      // img = src{=data.albums.items[0].images[0].url}
-      />
-      {/* <Artist/> */}
-      {/* <Album title="Testy Mc Testyface igen !"
-      description = "massor med viktig text"
-      name = {data.albums.items[1].name}/>/> */}
+      {
+        data.albums.items.map((items) => {
+          // console.log('items',items)
+          return <Album key={items.id} albumDetails={items}/>
+        })
+      }
       
     </section>
     
--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,28 +1,22 @@
 import React from "react";
-//  export const Album = () => {
-  //  return (
-    //      <section className='Container'>
-    //      <div>
-//        <h1>Testy Mc Testyface!</h1>
-//        <p>This is a test</p>
-//        <p>Testy testy test</p>
-//        <img src="https://www.placecage.com/200/300" alt="Nicolas Cage"/>
-//      </div>
-//   </section>
-//  )
-//  };
 
 
 const Album = (props) => {
-  console.log(props)
+  console.log('props',props)
+
   return (
          <section className='Container'>
+           <div className="album-card">
          <div>
-           <h1>{props.name}</h1>
-           <p>{props.description}</p>
-           <p>{props.artist}</p>
-          <img src="https://www.placecage.com/200/300" alt="Nicolas Cage"/>
-          <img src="{props.img}" alt="Nicolas Cage"/>
+           <div className="img-container">
+          <img src={props.albumDetails.images[1].url} alt="Album cover"/>
+           </div>
+           <h1>{props.albumDetails.name}</h1>
+           <p>{props.albumDetails.artists[0].name}</p>
+         <a href="{props.albumDetails.ex}">link to album</a>
+        </div>
+
+
          </div>
        </section>
      )
