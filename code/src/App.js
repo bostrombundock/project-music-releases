aab232a6808e2f7c13a3c65f29605f95ed172041
bostrombundock <sbostrom@ymail.com>
2022-03-10 14:27:09 +0100

    Co-authored-by: Frida Axelsson <Nosslexa@users.noreply.github.com>

--- code/src/App.js
+++ code/src/App.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import data from './data.json'
 
-// import {Album} from './components/Album'
 
 import Album from './components/Album'
 
-console.log(data)
 
 export const App = () => {
   return (
--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,18 +1,5 @@
 import React from "react";
 
-// export const Album = () => {
-// return (
-//     <section className='Container'>
-//     <div>
-//       <h1>Testy Mc Testyface!</h1>
-//       <p>This is a test</p>
-//       <p>Testy testy test</p>
-//       <img src="https://www.placecage.com/200/300" alt="Nicolas Cage"/>
-//     </div>
-//   </section>
-// )
-// };
-
 
 const Album = () => {
     return (
@@ -28,3 +15,5 @@ const Album = () => {
     };
     
     export default Album;
+
+    
