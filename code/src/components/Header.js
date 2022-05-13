2cb7a47e1f9d3a1dcba65848aa108048d7f464cf
frida axelsson <axelsson.frida@gmail.com>
2022-05-13 15:30:27 +0200

    added styling

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,7 +1,9 @@
 import React from 'react'
 const Header =() => {
 return (
-   <h1> New albums & singles </h1> 
+   <div className='header-container'>
+   <h1 className='header-text'> New albums & singles </h1> 
+   </div>
 )
 
 }
